ee9f74f52cafd53f4be50f66873c9064eee24885
Andres <97533377+Afmastro@users.noreply.github.com>
2022-08-17 13:19:09 -0300

    Add files via upload

--- ventiladores.py
+++ ventiladores.py
@@ -6,15 +6,25 @@ user="root",password="")
 
 app=Flask(__name__)
 
-@app.route('/',methods=['GET','UPDATE'])
+@app.route('/',methods=['GET'])
 def Ventiladores():
     cursor=mydb.cursor()
-    cursor.execute("select conexion from ventiladores;")
-    resultado=cursor.fetchall()
-    print(resultado)
+    cursor.execute("select * from ventiladores;")
+    resultados=cursor.fetchall()
+    print(resultados)
+    resultado={"ventiladores":[]}
+    resultado["ventiladores"].append({"id":resultados[0][0]})
+    resultado["ventiladores"].append({"estado":resultados[0][1]})
+    resultado["ventiladores"].append({"conexion":resultados[0][2]})
+    resultado["ventiladores"].append({"nombre":resultados[0][3]})
+    resultado["ventiladores"].append({"velocidad":resultados[0][4]})
+    resultado["ventiladores"].append({"timer":resultados[0][5]})
+    resultado["ventiladores"].append({"sleep":resultados[0][6]})
+    return resultado
+    # return {'resultado':resultados}
     cursor.close()
     mydb.close()
-    return {'VENTILADORES':resultado}
+
 
 if __name__=='__main__':
     app.run(debug=True)
