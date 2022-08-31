7dc4a7e2153ebaef5dcffa1c2054a39e70c194e5
Eliana <enicotra83@gmail.com>
2022-08-31 20:40:37 -0300

    se agrega logica a pantalla principal

--- Frontend/src/components/listaItem/CardEncendido.jsx
+++ Frontend/src/components/listaItem/CardEncendido.jsx
@@ -1,64 +1,62 @@
-import "./listaItem.css";
-import { Link } from "react-router-dom";
-import imgVentilador from "../../assets/statics/vent-rojo.png";
-import imgAireGris from "../../assets/statics/aire-gris.png";
-import vel from "../../assets/vel.png";
-import temp from "../../assets/temp.png";
-import sleep from "../../assets/sleep.png";
+import './listaItem.css'
+import { Link } from 'react-router-dom'
+import imgVentilador from '../../assets/statics/vent-rojo.png'
+import imgAireGris from '../../assets/statics/aire-gris.png'
+import vel from '../../assets/vel.png'
+import temp from '../../assets/temp.png'
+import sleep from '../../assets/sleep.png'
 
 export default function CardEncendido(props) {
-  const { dispositivo } = props;
+  const { dispositivo } = props
   const handleEditar = () => {
-    alert("Aca se abrira el modal con las configuraciones");
-  };
+    alert('Aca se abrira el modal con las configuraciones')
+  }
 
   return (
-    <Link to={`/dispositivo/:${dispositivo[0]}`} className="link">
-      <div className="itemActivo">
-        <div className="itemPrincipal">
-          <div className="contenedorImg">
+    <Link to={`/dispositivo/:${dispositivo[0]}`} className='link'>
+      <div className='itemActivo'>
+        <div className='itemPrincipal'>
+          <div className='contenedorImg'>
             {dispositivo[8] ? (
-              <img src={imgAireGris} alt="" className="imgItem" />
+              <img src={imgAireGris} alt='' className='imgItem' />
             ) : (
-              <img src={imgVentilador} alt="" className="imgItem" />
+              <img src={imgVentilador} alt='' className='imgItem' />
             )}
           </div>
-          <div className="principalTxt">
+          <div className='principalTxt'>
             <h2>{dispositivo[3]}</h2>
             {dispositivo[8] ? (
-              <p className="infoTxt">Aire Acondicionado</p>
+              <p className='infoTxt'>Aire Acondicionado</p>
             ) : (
-              <p className="infoTxt">Ventilador</p>
+              <p className='infoTxt'>Ventilador</p>
             )}
           </div>
-          <span className="material-icons puntitos" onClick={handleEditar}>
+          <span className='material-icons puntitos' onClick={handleEditar}>
             more_vert
           </span>
         </div>
-        <div className="itemActivoDetalles">
+        <div className='itemActivoDetalles'>
           {dispositivo[8] ? (
             <button>
-              {" "}
-              <img src={temp} alt="" />
-              <p>24°</p>
+              <img src={temp} alt='' />
+              <p>{dispositivo[4]}°</p>
             </button>
           ) : (
             <button>
-              <img src={vel} alt="" />
-              <p>4</p>
+              <img src={vel} alt='' />
+              <p>{dispositivo[4]}</p>
             </button>
           )}
-
-          <button className="indicadorSleep">
-            <img src={sleep} alt="" />
-            <p>4</p>
+          <button className='indicadorSleep'>
+            <img src={sleep} alt='' />
+            <p>{dispositivo[dispositivo.length-1]}</p>
           </button>
-          <div className="encendido">
-            <span className="material-icons">power_settings_new</span>
-            <p className="infoTxt">ON</p>
+          <div className='encendido'>
+            <span className='material-icons'>power_settings_new</span>
+            <p className='infoTxt'>ON</p>
           </div>
         </div>
       </div>
     </Link>
-  );
+  )
 }
